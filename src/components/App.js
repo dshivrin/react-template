6f5cab3d3c6be76e85f6d488b7706d599641bf55
Dima Shivrin <dshivrin@gmail.com>
2019-12-10 13:42:09 +0200

    Init

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  // state = { displayBio: false };
+
+  // constructor(){
+  //     super();
+  //     this.state = {displayBio: false};
+
+  //     this.toggleDisplay = this.toggleDisplay.bind(this);
+  // }
+
+  render() {
+    return <div>React App</div>;
+  }
+}
+
+export default App;
